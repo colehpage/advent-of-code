5c0a5533abb95cfe3723c2552b45b12e202a6380
CHP <colehpage@gmail.com>
2024-01-18 23:00:31 -0500

    god7p2

--- go/2023/7/main.go
+++ go/2023/7/main.go
@@ -70,17 +70,57 @@ func p1(input io.Reader) int {
 	})
 
 	for i, hand := range hands {
-		log.Println(hand.hand)
 		sum += (i + 1) * hand.bid
 	}
 
-	// need to sort all the hands by their hand type, and then within their hand types then loop
-	// through and apply the ranks and bid amounts and sum them
 	return sum
 }
 
 func p2(input io.Reader) int {
-	return 33
+	sum := 0
+
+	lines := aoc.ReaderToStrings(input)
+
+	var hands []Hand
+
+	for i := range lines {
+		split := strings.Split(lines[i], " ")
+		hands = append(hands, Hand{hand: split[0], bid: aoc.StringToInt(split[1])})
+	}
+
+	// compare first by hand type
+	// second by card values starting with first card
+
+	sort.Slice(hands, func(i, j int) bool {
+		handA := hands[i].hand
+		handB := hands[j].hand
+
+		scoreA := handScore2(handA)
+		scoreB := handScore2(handB)
+
+		if scoreA[0] == scoreB[0] {
+			if scoreA[1] == scoreB[1] {
+				// hand types identical
+				// check scores of individual cards in order
+				for i := 0; i < len(handA); i++ {
+					if cardValue2(rune(handA[i])) == cardValue2(rune(handB[i])) {
+						continue
+					} else {
+						return cardValue2(rune(handA[i])) < cardValue2(rune(handB[i]))
+					}
+				}
+			} else {
+				return scoreA[1] < scoreB[1]
+			}
+
+		}
+		return scoreA[0] < scoreB[0]
+	})
+
+	for i, hand := range hands {
+		sum += (i + 1) * hand.bid
+	}
+	return sum
 }
 
 type Hand struct {
@@ -112,6 +152,47 @@ func handScore(hand string) [2]int {
 
 func cardValue(c rune) int {
 	cards := "23456789TJQKA"
+	for i, card := range cards {
+		if card == rune(c) {
+			return i
+		}
+	}
+	return -1
+}
+
+// should probably combine these parts and make things a bit dryer....
+
+func handScore2(hand string) [2]int {
+	faces := make([]int, 13)
+	jokers := 0
+
+	for _, c := range hand {
+		if c == 'J' {
+			jokers++
+		} else {
+			faces[cardValue2(c)]++
+		}
+	}
+
+	sort.Slice(faces, func(i, j int) bool {
+		return faces[i] < faces[j]
+	})
+
+	score := [2]int{}
+
+	copy(score[:], faces[11:])
+
+	for i, j := 0, len(score)-1; i < j; i, j = i+1, j-1 {
+		score[i], score[j] = score[j], score[i]
+	}
+
+	score[0] += jokers
+
+	return score
+}
+
+func cardValue2(c rune) int {
+	cards := "J23456789TQKA"
 	for i, card := range cards {
 		if card == rune(c) {
 			return i
